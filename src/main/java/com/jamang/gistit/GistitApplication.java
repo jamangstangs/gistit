46d659d404582b71a2297c97fd408a4716156fb1
jamangstangs <jamangsong@gmail.com>
2022-09-06 21:31:28 +0900

    project init

---
+++ src/main/java/com/jamang/gistit/GistitApplication.java
@@ -0,0 +1,13 @@
+package com.jamang.gistit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GistitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GistitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamang/gistit/GistitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamang.gistit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GistitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
